8ab3a856999b3fb935010e8994cd6069dd24198e
adnensaid <adnensaid83@gmail.com>
2021-05-03 14:09:35 +0100

    webify first commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src/index.js")
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }  
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './src/assets/images/*',
+          to: 'assets/images/[name][ext]',
+        },
+      ],
+    }),  
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "./src/index.html"),
+      chunks: ["main"]
+    })
+  ],
+  stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    open: false,
+    contentBase: "./dist",
+    inline: true,
+    port: 4000
+  }
+};
