f4d50fc2f33b17891b2a95c83a5a8e877c21adf5
adnensaid <adnensaid83@gmail.com>
2021-05-04 15:25:57 +0100

    version tablette and animation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import "./assets/styles/styles.scss";
 import "./index.scss";
+
+AOS.init();
+/*let dernierePositionScrollConnue = 0;
+let ticking = false;
+
+function faireQuelqueChose(positionScroll) {
+  const expertises = document.querySelector(".expertises-content");
+  console.log(expertises);
+  expertises.classList.toggle('expertise-animation');
+}
+
+window.addEventListener('scroll', function(e){
+  dernierePositionScrollConnue = window.scrollY;
+  console.log(dernierePositionScrollConnue);
+  if(!ticking) {
+    window.requestAnimationFrame(function(){
+      faireQuelqueChose(dernierePositionScrollConnue);
+    })
+  }
+}) */
